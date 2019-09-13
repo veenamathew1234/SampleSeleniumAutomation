e0e946a857a9563095dcae13705da8584998cf18
Veena Mathew <veena.mathew@knolskape.com>
2019-09-14 00:23:29 +0530

    Adding an object factory

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,17 +1,21 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+
+import utils.ObjectFactory;
 import utils.StartUp;
 
 public class HomePage extends StartUp {
 
 	//WebDriver driver;
-	
+	ObjectFactory objmap;
 	public HomePage(WebDriver driver)
 	{
 		this.driver=driver;
+		this.objmap=new ObjectFactory(System.getProperty("user.dir")+"/src/main/java/uiMap/HomePage.properties");
 	}
 	ShoppingCart sc=new ShoppingCart(driver);
 	
@@ -24,13 +28,23 @@ public void launchPage(){
 
 //Function to click on Buy Now button
 	public void clickOnBuyNow(){
-		
-		driver.findElement(By.xpath("//a[contains(@class,'btn buy')]")).click();
+		try
+		{
+			driver.findElement(objmap.getLocator("btn_BuyNow")).click();
 		
 		/*After clicking on Buy Now button , the user should be displayed the shopping cart
 		 * 
 		 * 
 		 */
 		Assert.assertEquals(sc.verifyShoppingCart(), true, "Shopping cart not displayed after BuyNow click");
+		}
+		catch(NoSuchElementException ne)
+		{
+			Assert.assertNull(ne);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
